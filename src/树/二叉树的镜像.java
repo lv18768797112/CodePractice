0167e88ed6962c37c42894fea32481d57538e8a9
sout <lv18768797112@163.com>
2020-07-18 21:11:53 +0800

    树

---
+++ src/树/二叉树的镜像.java
@@ -0,0 +1,40 @@
+package 树;
+//请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+//
+//例如输入：
+//
+//     4
+//   /   \
+//  2     7
+// / \   / \
+//1   3 6   9
+//
+//镜像输出：
+//     4
+//   /   \
+//  7     2
+// / \   / \
+//9   6 3   1
+//
+//示例 1：
+//输入：root = [4,2,7,1,3,6,9]
+//输出：[4,7,2,9,6,3,1]
+//
+//限制：
+//0 <= 节点个数 <= 1000
+
+public class 二叉树的镜像 {
+    public TreeNode mirrorTree(TreeNode root) {
+        mirrorNode(root);
+        return root;
+    }
+    public void mirrorNode(TreeNode root){
+        if(root==null)
+            return;
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        mirrorNode(root.left);
+        mirrorNode(root.right);
+    }
+}
