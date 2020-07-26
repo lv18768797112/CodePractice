e12683b274b4660789e847d356d28d67c9655396
sout <lv18768797112@163.com>
2020-07-26 23:44:52 +0800

    栈和队列

---
+++ LeetCode/src/栈和队列/最小栈.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+//package 栈和队列;
+//设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+//push(x) —— 将元素 x 推入栈中。
+//pop() —— 删除栈顶的元素。
+//top() —— 获取栈顶元素。
+//getMin() —— 检索栈中的最小元素。
+// 
+//输入：
+//["MinStack","push","push","push","getMin","pop","top","getMin"]
+//[[],[-2],[0],[-3],[],[],[],[]]
+//
+//输出：
+//[null,null,null,null,-3,null,0,-2]
+//
+//提示：
+//pop、top 和 getMin 操作总是在 非空栈 上调用。
+
+public class 最小栈 {
+    private Stack<Integer> stack;
+    private Stack<Integer> min_stack;
+    public 最小栈(){
+        stack = new Stack<>();
+        min_stack = new Stack<>();
+    }
+    public void push(int x) {
+        stack.push(x);
+        if(min_stack.isEmpty() || x <= min_stack.peek())
+            min_stack.push(x);
+    }
+    public void pop() {
+        if(stack.pop().equals(min_stack.peek()))
+            min_stack.pop();
+    }
+    public int top() {
+        return stack.peek();
+    }
+    public int getMin() {
+        return min_stack.peek();
+    }
+}
